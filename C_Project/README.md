5f5ea62c89abaf9531f1071ebaf9462c344e9679
lakshay khanna <lakshay1116.cse19@chitkara.edu.in>
2021-09-10 21:12:25 +0530

    C Project

---
+++ C_Project/README.md
@@ -0,0 +1,30 @@
+
+# Re-Writing Shell Commands
+
+
+
+## Overview
+  Implementing Shell Commands purely in C language. Doing commands(i.e cat ,chmod,mkdir,echo). I am  interested to know about the backend working of these commands . so, i decided to implement it in my own way.
+ 
+### Features 
+   Implement Shell Commands like :
+ - cat
+ - chmod
+ - mkdir
+ - echo
+ ### Preview
+ Screencaps of the completed will be added soon.
+ ### Further features to add:
+In future more commands will be added like cp,mv,touch,.etc.
+
+ Coordinated by:
+
+Mr. Kumar Gaurav Sir
+
+Mr. Ritesh Ranjan Sir
+
+Miss. Anuinder Mam
+
+
+
+
---
+++ C_Project/cat.c
@@ -0,0 +1,56 @@
+/* Rewriting cat command.
+ * @lakshay khanna (1910991116)
+ * C Project
+ *Compilation: cat.c
+ * Execution: ./a.out
+ * source :-GeeksforGeeks
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[])
+{  
+    char *a1 = (char*) malloc (sizeof(char));
+    int sz, fd,cont=0, cont1=0;
+    char *b1 = (char*) malloc (sizeof(char));
+    //char *a2 = (char*) malloc (sizeof(char));
+    char * a2;
+    char *b2 = (char*) malloc (sizeof(char));
+
+    // NO PARAMETERS
+    while (argc == 1){      
+        sz=read(0, a1, 1);
+        b1[cont]=a1[0];
+
+        if(b1[cont]=='\n'){
+            write(1,b1,cont);
+            write(1,"\n",1);
+            b1=NULL;            
+        }
+
+        cont=cont+1;
+        b1=(char*) realloc(b1, sizeof(char)*cont);
+      }
+
+    // 1 PARAMETER (FILE)   /*Here is the problem*/
+    if (argc > 1){
+
+        fd=open(argv[1],O_RDONLY);
+        a2=fgetc(fd);
+
+        while (a2 != EOF){
+            b2[cont1]=a2;
+            cont1=cont1+1;
+            b2=(char*) realloc (b2, sizeof(char)*cont1+1);
+            a2=fgetc(fd);
+        }
+
+        write(1,b2,cont);
+        b2=NULL;
+        close(fd);  
+    }
+
+    return 0;
+}
